0481f4f0b39e1ada5426bbb6afa987119169579f
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-10-23 01:07:55 +0200

    Add files via upload

---
+++ Inicijalno3/Inicijalno3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalno3
+{
+    internal class Program
+    {
+        class Vozilo
+        {
+            bool jeLiLeti, jeLiPlovi;
+
+            public bool JeLiLeti { get => jeLiLeti; set => jeLiLeti = value; }
+            public bool JeLiPlovi { get => jeLiPlovi; set => jeLiPlovi = value; }
+            public void KudaVozi()
+            {
+                Console.WriteLine("Jeli leti: " + JeLiLeti + "\n" + "Jeli plovi: " + JeLiPlovi);
+            }
+        }
+        class Brod : Vozilo
+        {
+            public Brod()
+            {
+                JeLiPlovi = true;
+            }
+        }
+        class Zrakoplov : Vozilo
+        {
+            public Zrakoplov()
+            {
+                JeLiLeti = true;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Brod brod = new Brod();
+            Zrakoplov zrakoplov = new Zrakoplov();
+
+            Console.WriteLine("Brod");
+            brod.KudaVozi();
+            Console.WriteLine("Zrakoplov");
+            zrakoplov.KudaVozi();
+
+            Console.ReadKey();
+        }
+    }
+}
